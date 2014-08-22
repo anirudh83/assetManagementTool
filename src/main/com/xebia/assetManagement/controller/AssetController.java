1e953e148e270149984df804383e6cbad14a8a03
anirudh <anirudh@panthacorp.com>
2014-08-22 14:56:55 +0530

    Added model and jsp

---
+++ src/main/com/xebia/assetManagement/controller/AssetController.java
@@ -0,0 +1,7 @@
+package com.xebia.assetManagement.controller;
+
+/**
+ * Created by anirudh on 22/08/14.
+ */
+public class AssetController {
+}
---
+++ src/main/com/xebia/assetManagement/model/User.java
@@ -0,0 +1,7 @@
+package com.xebia.assetManagement.model;
+
+/**
+ * Created by anirudh on 22/08/14.
+ */
+public class User {
+}
